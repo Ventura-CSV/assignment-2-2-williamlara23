bb2dac59286ba0f2791c1a06689e31f0da891aa1
williamlara23 <wilberth_lara1@my.vcccd.edu>
2023-08-26 19:34:14 -0700

    1st commit attempt

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ def main():
     Use the same variables: celcius fahrenheit 
     ##################################################
     """
-
+    celcius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celcius * 9/5) + 32
+    print(f'Fahrenheit:\t {fahrenheit:.2f}')
     """
     ########################################
     # Do not delete the return statement
